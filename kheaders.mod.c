7a3b650f550eebe3a44b6b4ecf0ac2131a84e4d9
shinyohan <rokaf72781110@gmail.com>
2023-11-11 01:12:28 +0900

    6th ass

---
+++ kheaders.mod.c
@@ -0,0 +1,40 @@
+#include <linux/build-salt.h>
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(.gnu.linkonce.this_module) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+MODULE_INFO(intree, "Y");
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section(__versions) = {
+	{ 0xed93c0b3, "module_layout" },
+	{ 0xdeb089a7, "sysfs_remove_bin_file" },
+	{ 0x74dbd78d, "sysfs_create_bin_file" },
+	{ 0xc4dc934e, "kernel_kobj" },
+	{ 0x69acdf38, "memcpy" },
+	{ 0xbdfb6dbb, "__fentry__" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "F18C84EA029286DACD7A24E");
